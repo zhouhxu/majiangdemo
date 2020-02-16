b1e0dac070bb7d4bb2e06784efe31bc60e370524
zhouyb <zhouhxu@yeah.net>
2020-02-16 17:27:09 +0800

    repo

---
+++ src/main/java/cn/edu/hxu/majiangdemo/MajiangdemoApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hxu.majiangdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajiangdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajiangdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/hxu/majiangdemo/MajiangdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hxu.majiangdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
