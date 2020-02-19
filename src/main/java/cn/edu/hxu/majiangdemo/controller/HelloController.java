b6b8f782c743f7a2d3b7078cb73cc39c02d76b40
zhouyb <zhouhxu@yeah.net>
2020-02-19 16:41:38 +0800

    add flyway migration support

--- src/main/java/cn/edu/hxu/majiangdemo/controller/HelloController.java
+++ src/main/java/cn/edu/hxu/majiangdemo/controller/HelloController.java
@@ -1,12 +1,21 @@
 package cn.edu.hxu.majiangdemo.controller;
 
+import cn.edu.hxu.majiangdemo.mapper.UserMapper;
+import cn.edu.hxu.majiangdemo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
+
+    @Autowired
+    UserMapper userMapper;
 
     @GetMapping("/hello")
     public String hello(@RequestParam(name="name", required = false,defaultValue = "World") String name, Model model){
@@ -15,7 +24,18 @@ public class HelloController {
     }
 
     @GetMapping("/index")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie: cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findUserByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+            }
+            break;
+        }
         return "index";
     }
 }
--- src/main/java/cn/edu/hxu/majiangdemo/mapper/UserMapper.java
+++ src/main/java/cn/edu/hxu/majiangdemo/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package cn.edu.hxu.majiangdemo.mapper;
 import cn.edu.hxu.majiangdemo.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -10,5 +12,6 @@ public interface UserMapper {
     @Insert("Insert into user (account_id,name,token,gmt_create,gmt_updated) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtUpdate})")
     int save(User user);
 
-
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findUserByToken(@Param("token") String token);
 }
